e43ba28dac53f400bef31077b5f53b12a83297d1
Plyncesilva <plyncesilva@gmail.com>
2022-12-15 10:31:15 +0000

    scp

--- ssh.py
+++ ssh.py
@@ -1,8 +1,14 @@
 import paramiko
+from scp import SCPClient
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
 ssh.connect("10.0.2.15", password="kali")
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("wget https://raw.githubusercontent.com/Plyncesilva/TMA/main/ssh.py")
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("echo gotit")
+
+scp = SCPClient(ssh.get_transport())
+scp.put("ssh.py")
+
+
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("cat ssh.py")
